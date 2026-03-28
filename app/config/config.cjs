d00394ade939d9774750cf7013a35fb899068296
Mikozzun <146335969+Mikozzun@users.noreply.github.com>
2026-03-29 01:43:48 +0300

    pleaseletmebefinal

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -1,5 +1,5 @@
-// app/config/config.cjs
 require('dotenv').config();
+
 module.exports = {
   development: {
     dialect: 'sqlite',
@@ -7,18 +7,18 @@ module.exports = {
   },
   production: {
     dialect: 'postgres',
+    host: process.env.DATABASE_HOST || 'localhost',
+    port: process.env.DATABASE_PORT || 5432,
+    database: process.env.DATABASE_NAME || 'postgres',
+    username: process.env.DATABASE_USERNAME || 'postgres',
+    password: process.env.DATABASE_PASSWORD || 'postgres',
+  },
+  test: {
+    dialect: 'postgres',
     database: process.env.DATABASE_NAME,
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     port: process.env.DATABASE_PORT,
     host: process.env.DATABASE_HOST,
-  },
-  test: {
-  dialect: 'postgres',
-  database: process.env.DATABASE_NAME,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  port: process.env.DATABASE_PORT || 5432,
-  host: process.env.DATABASE_HOST,
   },
 };
